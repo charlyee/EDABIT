1d997ea84730cc61028b2995763bbb50923b8faf
CharlZ <charles.ceo2020@gmail.com>
2020-03-30 01:31:53 -0600

    wknd 1 assignment

---
+++ WKND1.js
@@ -0,0 +1,24 @@
+// ASSIGNMENT 1: Write a function that returns the first n vowels of a string.
+
+function firstNVowels(s, n) {
+    var regex = /[aeiou]/gi;
+    var vowel = s.match(regex);
+    var result = "";
+    if(vowel.length >= n){
+        for(var i = 0; i < n; i++){
+             result += vowel[i];    
+    } 
+    return result;
+    } else {
+        return "invalid";                
+        }
+}
+
+
+
+// ASSIGNMENT 2: Create a function that takes an integer n and reverses it
+function rev(n) {
+	n = n + "";
+	return n.split('').reverse().join('');
+}
+console.log(rev(987654));
